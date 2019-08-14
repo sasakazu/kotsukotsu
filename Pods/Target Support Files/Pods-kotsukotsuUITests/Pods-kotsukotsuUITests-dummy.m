d44c631cb48b6164dd047b5b6754b4981051d480
kazuyasasakura <kazuya.sasakura@gmial.com>
2019-08-14 18:52:32 +0900

    first commit

---
+++ Pods/Target Support Files/Pods-kotsukotsuUITests/Pods-kotsukotsuUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_kotsukotsuUITests : NSObject
+@end
+@implementation PodsDummy_Pods_kotsukotsuUITests
+@end
---
+++ kotsukotsu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kotsukotsu
+//
+//  Created by 笹倉一也 on 2019/08/14.
+//  Copyright © 2019 笹倉一也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
